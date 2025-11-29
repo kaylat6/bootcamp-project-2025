41155e8fb3da24e8c26f0b7dc7e4c7296b09c507
kaylat6 <kayla.tran616@gmail.com>
2025-11-29 12:47:39 -0800

    feat: add content to src/app/blog/page.tsx

--- bootcamp-milestone-2/src/app/blog/page.tsx
+++ bootcamp-milestone-2/src/app/blog/page.tsx
@@ -1,7 +1,23 @@
-/*
-import BlogPreview from '@/components/blogPreview';
-    //somewhere in the return statement
-    {blogs.map(blog => 
-      <BlogPreview /> /
-	)}
-*/
+import React from 'react';
+import style from './blog.module.css';
+import BlogPreview from '../../components/blogPreview';
+import blogs from '../blogData';
+
+export default function BlogPage() {
+  return (
+    <main className={style.container}>
+      {blogs.map(blog => (
+        <BlogPreview
+          key={blog.slug}
+          title={blog.title}
+          date={blog.date}
+          description={blog.description}
+          image={blog.image}
+          imageAlt={blog.imageAlt}
+          slug={blog.slug}
+      />
+    ))}
+    </main>
+  );
+}
+
--- bootcamp-milestone-2/src/components/blogPreview.tsx
+++ bootcamp-milestone-2/src/components/blogPreview.tsx
@@ -1,19 +1,17 @@
 import React from 'react';
-import Image from 'next/image';
-import style from './blogPreview.module.css';
+import type {Blog} from "../blogData";
+import Link from "next/link";
+import style from './blogPreview.module.css'
 
-export default function BlogPreview() {
+export default function BlogPreview({title, date, description, image, imageAlt, slug}: Blog) {
   return (
-		// replace everything between the <div> & </div> tags
-		// with your code from earlier milestones
-    <div className={style.div}>
-      <h3> Campus cat </h3>
+    <div className={style.blogContainer}>
+      <h3> {title} </h3>
       <div>
-        <img src="IMG_7307.jpg" alt="Campus cat" />
-			<Image src="IMG_7307.jpg" alt="img" width={500} height={500} ></Image>
-        <p>Campus cat visiting our dorm! We gave him some cat treats and now he keeps coming back.</p>
-		<p>Posted on 9/16/25</p>
+        <img src={image} alt={imageAlt} />
+        <p>{description}</p>
+        <p>{date}</p>
       </div>
-	</div>
+    </div>
   );
 }
