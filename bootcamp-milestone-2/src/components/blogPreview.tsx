158f9a422cd38d7fefc9f3f0b55de61b98128311
kaylat6 <kayla.tran616@gmail.com>
2025-12-02 21:40:31 -0800

    feat: add content to src/components/blogPreview.tsx

--- bootcamp-milestone-2/src/components/blogPreview.tsx
+++ bootcamp-milestone-2/src/components/blogPreview.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
-import type {Blog} from "../blogData";
+import type {Blog} from "../database/blogSchema";
 import Link from "next/link";
 import style from './blogPreview.module.css'
 
 export default function BlogPreview({title, date, description, image, imageAlt, slug}: Blog) {
   return (
     <div className={style.blogContainer}>
-      <h3> {title} </h3>
-      <div>
-        <img src={image} alt={imageAlt} />
+      <img src={image} alt={imageAlt} className = {style.blogImage}/>
+      <div className={style.blogInfo}>
+        <h3 className={style.blogTitle}>{title}</h3>
         <p>{description}</p>
         <p>{date}</p>
       </div>
