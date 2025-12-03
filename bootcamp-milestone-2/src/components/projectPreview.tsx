95750aa2dd67b8c0e5891f4ebaf517ea64fc4bad
kaylat6 <kayla.tran616@gmail.com>
2025-12-02 21:51:53 -0800

    feat: add content to src/components/projectPreview.tsx

--- bootcamp-milestone-2/src/components/projectPreview.tsx
+++ bootcamp-milestone-2/src/components/projectPreview.tsx
@@ -1,16 +1,24 @@
-import React from 'react';
-import type {Project} from "../database/projectSchema";
+import React from "react";
+import type { Project } from "../database/projectSchema";
 import Link from "next/link";
-import style from './projectPreview.module.css'
+import style from "./projectPreview.module.css";
 
-export default function ProjectPreview({title, description, link, image, image_alt}: Project) {
+export default function ProjectPreview({
+  title,
+  description,
+  link,
+  image,
+  image_alt,
+}: Project) {
   return (
     <div className={style.projectContainer}>
       <h3> {title} </h3>
       <div>
         <img src={image} alt={image_alt} />
         <p>{description}</p>
-        <a href={link} className={style.info}>Learn more here!</a>
+        <a href={link} className={style.info}>
+          Learn more here!
+        </a>
       </div>
     </div>
   );
